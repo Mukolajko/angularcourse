874be5c140ca33350e4ef7705ffb880c32094c00
Mukolajko <borusyuk_kolya@ukr.net>
2016-04-07 10:41:20 +0300

    last fix

--- js/posts/controllers/AllPostsController.js
+++ js/posts/controllers/AllPostsController.js
@@ -1,4 +1,6 @@
 App.controller('AllPostsController',function($scope, Posts) {
     //make API call to get all posts
-    $scope.posts = Posts.get({}, function() {});
+    $scope.posts = Posts.query(function(posts){
+        console.log(posts);
+    });
 });
--- js/posts/controllers/SinglePostController.js
+++ js/posts/controllers/SinglePostController.js
@@ -1,4 +1,6 @@
 App.controller('SinglePostController',function($scope, $routeParams, Posts){
     //make API call to get post by id
-    $scope.post = Posts.findById({id: $routeParams.id}, function() {});
+    $scope.post = Posts.get({id: $routeParams.id}, function(post){
+        console.log(post)
+    });
 });
