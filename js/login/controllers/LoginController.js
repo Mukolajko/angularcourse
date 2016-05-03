9e7bcd2cb027b93ceef5e9a0e4e3ea9bd18d439c
Mukolajko <borusyuk_kolya@ukr.net>
2016-05-03 14:54:59 +0200

    remove unnessesary stuff

--- js/login/controllers/LoginController.js
+++ js/login/controllers/LoginController.js
@@ -3,44 +3,21 @@ App.controller('LoginController',function($scope, $http, $location, AuthService,
     $scope.password = "";
     $scope.error = null;
 
-    var allUsers = [];
-    var matchedUserId = null;
-    //get all users
-    $http({
-        method: "GET",
-        url: "http://angular.codeforges.com/api/wp-json/wp/v2/users/"
-    }).then(function(data){
-        allUsers = data.data || [];
-    }, (function(res){
-        console.log("Error getting all users");
-        console.log(res);
-    }));
     //login
     $scope.login = function() {
-        //reset id of each try
-        matchedUserId = null;
-        //find user in all users and get id
-        allUsers.forEach(function(user){
-            if (user.name == $scope.name) {matchedUserId = user.id;}
-        });
-
-        if (matchedUserId) {
-            var baseString = $base64.encode($scope.name + ":" +  $scope.password);
-            $http({
-                method: "POST",
-                url: "http://angular.codeforges.com/api/wp-json/wp/v2/users/" + matchedUserId,
-                headers: {
-                    "authorization": "Basic " + baseString
-                }
-            }).then(function(data){
-                AuthService.login(data.data);
-                $location.url("/");
-            }, (function(res){
-                $scope.error = "Password incorrect or server problem";
-            }));
-        }
-        else {
-            $scope.error = "User not found";
-        }
+        var baseString = $base64.encode($scope.name + ":" +  $scope.password);
+        $http({
+            method: "GET",
+            url: "http://angular.codeforges.com/api/wp-json/wp/v2/users/me",
+            headers: {
+                Authorization: "Basic " + baseString
+            }
+        }).then(function(data){
+            AuthService.login(data.data);
+            $location.url("/");
+        }, (function(res){
+            console.log(res);
+            $scope.error = "Username or Password incorrect.";
+        }));
     };
 });
