52b284c948b4f8333ebcfcdc2b1b523775c8b8ae
Mukolajko <borusyuk_kolya@ukr.net>
2016-05-03 15:45:19 +0200

    fix error show

--- js/login/controllers/LoginController.js
+++ js/login/controllers/LoginController.js
@@ -8,16 +8,22 @@ App.controller('LoginController',function($scope, $http, $location, AuthService,
         var baseString = $base64.encode($scope.name + ":" +  $scope.password);
         $http({
             method: "GET",
-            url: "http://angular.codeforges.com/api/wp-json/wp/v2/users/me",
+            url: "http://angular.codeforges.com/api/wp-json/wp/v2/users/me?_envelope",
             headers: {
                 Authorization: "Basic " + baseString
             }
         }).then(function(data){
-            AuthService.login(data.data);
-            $location.url("/");
+            if (data.data.status != 302) {
+                $scope.error = "Incorrect username or password";
+            }
+            else {
+                AuthService.login(data.data.body);
+                $location.url("/");
+            }
+
         }, (function(res){
             console.log(res);
-            $scope.error = "Username or Password incorrect.";
+            $scope.error = "Sorry we have some server problem";
         }));
     };
 });
